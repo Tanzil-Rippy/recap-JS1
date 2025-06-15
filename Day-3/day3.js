a73524bd4dc0e5232875ea8b6d88a7ee24837cbc
Tanzil-Rippy <syedtanjilrippy@gmail.com>
2025-06-15 13:17:34 +0600

    task 2 done

--- Day-3/day3.js
+++ Day-3/day3.js
@@ -1,12 +1,21 @@
-function vowelsCounter (str){
-    let count = 0;
-    newStr = str.toLowerCase();
-    let vowels = ['a', 'e', 'i', 'o', 'u'];
-for(let i= 0; i < newStr.length; i++) {
+//  task 1: Count the number of vowels in a string
+ function vowelsCounter(str) {
+  let count = 0;
+  newStr = str.toLowerCase();
+  let vowels = ["a", "e", "i", "o", "u"];
+  for (let i = 0; i < newStr.length; i++) {
     if (vowels.includes(newStr[i])) {
       count++;
     }
   }
   return count;
-};
-console.log(vowelsCounter("Hello World!")); 
+}
+console.log(vowelsCounter("Hello World!"));
+
+
+// task 2: Reverse a string
+
+function reverseString(str) {
+  return str.split("").reverse().join('');
+}
+console.log(reverseString("Hello World!"));
