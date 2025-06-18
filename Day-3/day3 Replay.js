471c2f93e7f7cd3b8f73d08f868c910a426a09bd
Tanzil-Rippy <syedtanjilrippy@gmail.com>
2025-06-18 23:01:39 +0600

    task 3

--- Day-3/day3 Replay.js
+++ Day-3/day3 Replay.js
@@ -20,3 +20,30 @@ function deepClone(obj) {
   }
   return clonedObj;
 }
+
+// 🧠 2. flattenArray(arr)
+// Write a function that flattens deeply nested arrays into one flat array.
+function flattenArray(arr) {
+  const result = [];
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      result.push(...flattenArray(item)); // Recursively flatten nested arrays
+    } else {
+      result.push(item); // Add
+    }
+  });
+  return result;
+}
+
+// 🧠 3. isPalindrome(str)
+// Check if a string is a palindrome, ignoring case and non-alphanumeric characters.
+
+function isPalindrome(str) {
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, ""); // Remove non-alphanumeric characters and convert to lowercase
+
+  const reversedStr = cleanedStr.split("").reverse().join(""); // Reverse the cleaned string
+
+    return cleanedStr === reversedStr; // Check if the cleaned string is equal to its reverse
+}
+console.log(isPalindrome("A man, a plan, a canal: Panama") ("race a car")); // true
+isPalindrome("race a car"); // false
